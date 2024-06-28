918407408090d32ec5ac51f5fb167cebc6fd968e
tjadekll <153326793+tjadekll@users.noreply.github.com>
2024-06-28 10:26:45 -0500

    Update Test.py
    added high, medium, low, very low, informational levels

--- Test.py
+++ Test.py
@@ -1,2 +1,21 @@
 user_input = input("Enter Expression:")
 result = eval(user_input) # Unsafe
+
+def hardcoded_credentials():
+  password = "my_secret_password"
+
+def weak_password_policy(user_password):
+  if len(user_password) < 8:
+    print("Weak password! Must be at least 8 characters.")
+
+import hashlib
+
+def insecure_cryptography(password):
+  salt = b'salt'
+  hashed_password = hashlib.md5(salt + password.encode()).hexdigest()
+
+def unsured_variable():
+  unused_var = 42
+
+def debug_print():
+  print("Debugging information")
