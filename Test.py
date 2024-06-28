e5a99b95c633ff8c02e0127ae73fbde60caeee51
tjadekll <153326793+tjadekll@users.noreply.github.com>
2024-06-28 13:03:24 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,12 +1,6 @@
 user_input = input("Enter Expression:")
 result = eval(user_input) # Unsafe
 
-import hashlib
-
-def insecure_cryptography(password):
-  salt = b'salt'
-  hashed_password = hashlib.md5(salt + password.encode()).hexdigest()
-
 def unsured_variable():
   unused_var = 42
 
